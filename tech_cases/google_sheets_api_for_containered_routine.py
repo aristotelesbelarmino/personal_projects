f0bdec7feff6688fbda36a0c7d920f3799c91d58
Aristoteles Belarmino <aristoteles@sincronizaeducacao.com.br>
2023-05-19 22:11:29 -0300

    test commit

--- tech_cases/google_sheets_api_for_containered_routine.py
+++ tech_cases/google_sheets_api_for_containered_routine.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
 
 import os.path
-import google
-
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
