12b270fa9e5eb526a13c24a7dce6e53604800c70
Aristoteles Belarmino <aristoteles@sincronizaeducacao.com.br>
2023-05-21 14:46:51 -0300

    Libs organize

--- google_sheets_api_for_containered_routine.py
+++ google_sheets_api_for_containered_routine.py
@@ -1,11 +1,11 @@
 from __future__ import print_function
-
-import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import os.path
+
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
